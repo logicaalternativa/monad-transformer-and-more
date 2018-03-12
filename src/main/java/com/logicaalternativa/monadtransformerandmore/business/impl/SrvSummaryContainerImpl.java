f35d0e3f7ba7465ff035a21c115f19c4d105fee1
jfcorugedo <jfcorugedo@gmail.com>
2018-03-12 11:33:02 +0100

    My implementation

--- src/main/java/com/logicaalternativa/monadtransformerandmore/business/impl/SrvSummaryContainerImpl.java
+++ src/main/java/com/logicaalternativa/monadtransformerandmore/business/impl/SrvSummaryContainerImpl.java
@@ -2,6 +2,9 @@ package com.logicaalternativa.monadtransformerandmore.business.impl;
 
 import static com.logicaalternativa.monadtransformerandmore.util.TDD.$_notYetImpl;
 
+import com.logicaalternativa.monadtransformerandmore.bean.Book;
+import com.logicaalternativa.monadtransformerandmore.bean.Chapter;
+import com.logicaalternativa.monadtransformerandmore.bean.Sales;
 import com.logicaalternativa.monadtransformerandmore.bean.Summary;
 import com.logicaalternativa.monadtransformerandmore.business.SrvSummaryContainer;
 import com.logicaalternativa.monadtransformerandmore.container.Container;
@@ -11,6 +14,10 @@ import com.logicaalternativa.monadtransformerandmore.service.container.ServiceBo
 import com.logicaalternativa.monadtransformerandmore.service.container.ServiceChapterContainer;
 import com.logicaalternativa.monadtransformerandmore.service.container.ServiceSalesContainer;
 import com.logicaalternativa.monadtransformerandmore.errors.Error;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class SrvSummaryContainerImpl implements SrvSummaryContainer<Error> {
 	
@@ -38,8 +45,21 @@ public class SrvSummaryContainerImpl implements SrvSummaryContainer<Error> {
 
 	@Override
 	public Container<Error, Summary> getSummary(Integer idBook) {
-		
-		return $_notYetImpl();
+
+		Container<Error, Book> book = this.srvBook.getBook(idBook);
+		Book value = book.getValue();
+
+		List<Chapter> chapters = value.getChapters().stream()
+				.map(idChapter -> this.srvChapter.getChapter(idChapter))
+				.map(container -> container.getValue())
+				.collect(Collectors.toList());
+
+		Sales sales = this.srvSales.getSales(idBook).getValue();
+
+
+		Summary summary = new Summary(value, chapters, Optional.of(sales), this.srvAuthor.getAuthor(value.getIdAuthor()).getValue());
+
+		return Container.value(summary);
 		
 	}
 
