f1f24d97c2b1d3224e08c2833593a981aa0cfee2
sucius <alvaro.pozo.cuenca@gmail.com>
2018-03-12 18:15:06 +0100

    Exercice with futures

--- src/main/java/com/logicaalternativa/monadtransformerandmore/business/impl/SrvSummaryFutureEitherImpl.java
+++ src/main/java/com/logicaalternativa/monadtransformerandmore/business/impl/SrvSummaryFutureEitherImpl.java
@@ -1,10 +1,15 @@
 package com.logicaalternativa.monadtransformerandmore.business.impl;
 
+import akka.util.Timeout;
 import com.logicaalternativa.monadtransformerandmore.bean.*;
+import com.logicaalternativa.monadtransformerandmore.container.Container;
+import scala.Option;
 import scala.Tuple2;
+import scala.concurrent.Await;
 import scala.concurrent.ExecutionContext;
 import scala.concurrent.Future;
 import scala.concurrent.Promise;
+import scala.concurrent.duration.Duration;
 import scala.util.Either;
 import akka.dispatch.ExecutionContexts;
 import akka.dispatch.Futures;
@@ -17,9 +22,15 @@ import com.logicaalternativa.monadtransformerandmore.service.future.ServiceBookF
 import com.logicaalternativa.monadtransformerandmore.service.future.ServiceChapterFutEither;
 import com.logicaalternativa.monadtransformerandmore.service.future.ServiceSalesFutEither;
 import com.logicaalternativa.monadtransformerandmore.util.Java8;
+import scala.util.Right;
+import scala.util.Try;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import static com.logicaalternativa.monadtransformerandmore.util.Java8.*;
 import static com.logicaalternativa.monadtransformerandmore.util.TDD.$_notYetImpl;
@@ -54,13 +65,58 @@ public class SrvSummaryFutureEitherImpl implements SrvSummaryFutureEither<Error>
         final Future<Either<Error, Book>> book = srvBook.getBook(idBook);
         final Future<Either<Error, Sales>> sales = srvSales.getSales(idBook);
         final Future<Either<Error, Author>> author = book.flatMap(b -> srvAuthor.getAuthor(b.right().get().getIdAuthor()), ec);
+        final Future<Iterable<Either<Error, Chapter>>> listChapters =
+                book.flatMap(b ->
+                Futures.sequence(b.right()
+                        .get()
+                        .getChapters()
+                        .stream()
+                        .map(l -> srvChapter.getChapter(l))
+                        .collect(Collectors.toList()), ec), ec);
 
 
-        Future<Tuple2<Either<Error, Book>, Either<Error, Sales>>> zipBook = book.zip(sales);
+        Timeout timeout = new Timeout(Duration.create(200, "milliseconds"));
+        Book bookComp = null;
+        try {
+            bookComp = Await.result(book, timeout.duration()).right().get();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
+        Sales salesComp = null;
 
+        try {
+            salesComp = Await.result(sales, timeout.duration()).right().get();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-        return null;
+        Author authorComp = null;
+
+        try {
+            authorComp = Await.result(author, timeout.duration()).right().get();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        List<Chapter> listChaptersComp = new ArrayList<>();
+
+        Iterable<Either<Error, Chapter>> eitherIterable = null;
+
+        try {
+            eitherIterable = Await.result(listChapters, timeout.duration());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        eitherIterable.iterator().forEachRemaining(chapEither -> listChaptersComp.add(chapEither.right().get()));
+
+        Summary summary = new Summary(bookComp, listChaptersComp, Optional.of(salesComp) ,authorComp);
+        Either<Error, Summary> either = new Right(summary);
+
+        Promise<Either<Error, Summary>> promise = Futures.promise();
+
+        return promise.success(either).future();
     }
 
 }
