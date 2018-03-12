650812eb9bd98e615a9a788d6f47f33feddf50f9
sucius <alvaro.pozo.cuenca@gmail.com>
2018-03-12 14:35:23 +0100

    workshop changes

--- src/main/java/com/logicaalternativa/monadtransformerandmore/business/impl/SrvSummaryContainerImpl.java
+++ src/main/java/com/logicaalternativa/monadtransformerandmore/business/impl/SrvSummaryContainerImpl.java
@@ -2,7 +2,7 @@ package com.logicaalternativa.monadtransformerandmore.business.impl;
 
 import static com.logicaalternativa.monadtransformerandmore.util.TDD.$_notYetImpl;
 
-import com.logicaalternativa.monadtransformerandmore.bean.Summary;
+import com.logicaalternativa.monadtransformerandmore.bean.*;
 import com.logicaalternativa.monadtransformerandmore.business.SrvSummaryContainer;
 import com.logicaalternativa.monadtransformerandmore.container.Container;
 import com.logicaalternativa.monadtransformerandmore.monad.MonadContainer;
@@ -12,35 +12,47 @@ import com.logicaalternativa.monadtransformerandmore.service.container.ServiceCh
 import com.logicaalternativa.monadtransformerandmore.service.container.ServiceSalesContainer;
 import com.logicaalternativa.monadtransformerandmore.errors.Error;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 public class SrvSummaryContainerImpl implements SrvSummaryContainer<Error> {
-	
-	private final ServiceBookContainer<Error> srvBook;
-	private final ServiceSalesContainer<Error> srvSales;
-	private final ServiceChapterContainer<Error> srvChapter;
-	private final ServiceAuthorContainer<Error> srvAuthor;
-	
-	private final MonadContainer<Error> m;
-	
 
-	public SrvSummaryContainerImpl(ServiceBookContainer<Error> srvBook,
-			ServiceSalesContainer<Error> srvSales,
-			ServiceChapterContainer<Error> srvChapter,
-			ServiceAuthorContainer<Error> srvAuthor, MonadContainer<Error> m) {
-		super();
-		this.srvBook = srvBook;
-		this.srvSales = srvSales;
-		this.srvChapter = srvChapter;
-		this.srvAuthor = srvAuthor;
-		this.m = m;
-	}
+    private final ServiceBookContainer<Error> srvBook;
+    private final ServiceSalesContainer<Error> srvSales;
+    private final ServiceChapterContainer<Error> srvChapter;
+    private final ServiceAuthorContainer<Error> srvAuthor;
+
+    private final MonadContainer<Error> m;
 
 
+    public SrvSummaryContainerImpl(ServiceBookContainer<Error> srvBook,
+                                   ServiceSalesContainer<Error> srvSales,
+                                   ServiceChapterContainer<Error> srvChapter,
+                                   ServiceAuthorContainer<Error> srvAuthor, MonadContainer<Error> m) {
+        super();
+        this.srvBook = srvBook;
+        this.srvSales = srvSales;
+        this.srvChapter = srvChapter;
+        this.srvAuthor = srvAuthor;
+        this.m = m;
+    }
 
-	@Override
-	public Container<Error, Summary> getSummary(Integer idBook) {
-		
-		return $_notYetImpl();
-		
-	}
+
+    @Override
+    public Container<Error, Summary> getSummary(Integer idBook) {
+
+        Container<Error, Book> book = srvBook.getBook(idBook);
+        Container<Error, Sales> sales = srvSales.getSales(idBook);
+        Container<Error, Author> author = srvAuthor.getAuthor(book.getValue().getIdAuthor());
+
+        List<Chapter> chapters =
+                book.getValue().getChapters().stream().map(c -> srvChapter.getChapter(c).getValue()).collect(Collectors.toList());
+
+        Summary summary = new Summary(book.getValue(), chapters, Optional.of(sales.getValue()), author.getValue());
+
+        return Container.value(summary);
+
+    }
 
 }
--- src/main/java/com/logicaalternativa/monadtransformerandmore/business/impl/SrvSummaryFutureEitherImpl.java
+++ src/main/java/com/logicaalternativa/monadtransformerandmore/business/impl/SrvSummaryFutureEitherImpl.java
@@ -1,12 +1,13 @@
 package com.logicaalternativa.monadtransformerandmore.business.impl;
 
+import com.logicaalternativa.monadtransformerandmore.bean.*;
+import scala.Tuple2;
 import scala.concurrent.Future;
 import scala.concurrent.Promise;
 import scala.util.Either;
 import akka.dispatch.ExecutionContexts;
 import akka.dispatch.Futures;
 
-import com.logicaalternativa.monadtransformerandmore.bean.Summary;
 import com.logicaalternativa.monadtransformerandmore.business.SrvSummaryFutureEither;
 import com.logicaalternativa.monadtransformerandmore.errors.Error;
 import com.logicaalternativa.monadtransformerandmore.monad.MonadFutEither;
@@ -16,36 +17,46 @@ import com.logicaalternativa.monadtransformerandmore.service.future.ServiceChapt
 import com.logicaalternativa.monadtransformerandmore.service.future.ServiceSalesFutEither;
 import com.logicaalternativa.monadtransformerandmore.util.Java8;
 
+import java.util.List;
+import java.util.Optional;
+
 import static com.logicaalternativa.monadtransformerandmore.util.Java8.*;
 import static com.logicaalternativa.monadtransformerandmore.util.TDD.$_notYetImpl;
 
 public class SrvSummaryFutureEitherImpl implements SrvSummaryFutureEither<Error> {
 
-	private final ServiceBookFutEither<Error> srvBook;
-	private final ServiceSalesFutEither<Error> srvSales;
-	private final ServiceChapterFutEither<Error> srvChapter;
-	private final ServiceAuthorFutEither<Error> srvAuthor;
-	
-	private final MonadFutEither<Error> m;
-	
-	
-	public SrvSummaryFutureEitherImpl(ServiceBookFutEither<Error> srvBook,
-			ServiceSalesFutEither<Error> srvSales,
-			ServiceChapterFutEither<Error> srvChapter,
-			ServiceAuthorFutEither<Error> srvAuthor,
-			MonadFutEither<Error> m) {
-		super();
-		this.srvBook = srvBook;
-		this.srvSales = srvSales;
-		this.srvChapter = srvChapter;
-		this.srvAuthor = srvAuthor;
-		this.m= m;
-	}
+    private final ServiceBookFutEither<Error> srvBook;
+    private final ServiceSalesFutEither<Error> srvSales;
+    private final ServiceChapterFutEither<Error> srvChapter;
+    private final ServiceAuthorFutEither<Error> srvAuthor;
 
-	@Override
-	public Future<Either<Error, Summary>> getSummary(Integer idBook) {
-		
-		return $_notYetImpl();
-	}
+    private final MonadFutEither<Error> m;
+
+
+    public SrvSummaryFutureEitherImpl(ServiceBookFutEither<Error> srvBook,
+                                      ServiceSalesFutEither<Error> srvSales,
+                                      ServiceChapterFutEither<Error> srvChapter,
+                                      ServiceAuthorFutEither<Error> srvAuthor,
+                                      MonadFutEither<Error> m) {
+        super();
+        this.srvBook = srvBook;
+        this.srvSales = srvSales;
+        this.srvChapter = srvChapter;
+        this.srvAuthor = srvAuthor;
+        this.m = m;
+    }
+
+    @Override
+    public Future<Either<Error, Summary>> getSummary(Integer idBook) {
+
+        final Future<Either<Error, Book>> book = srvBook.getBook(idBook);
+        final Future<Either<Error, Sales>> sales = srvSales.getSales(idBook);
+
+        Future<Tuple2<Either<Error, Book>, Either<Error, Sales>>> zipBook = book.zip(sales);
+
+
+
+        return null;
+    }
 
 }
